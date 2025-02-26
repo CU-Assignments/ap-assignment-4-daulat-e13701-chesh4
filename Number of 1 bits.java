01ba0694bc4a47f2ca8b88c3aa0c6711f1853646
chesh4 <cheshtajain12200420@gmail.com>
2025-02-26 21:37:18 +0530

    Update Number of 1 bits.java

--- Number of 1 bits.java
+++ Number of 1 bits.java
@@ -1,10 +1,8 @@
-public class Solution {
-    public int hammingWeight(int n) {
-        int count = 0;
-        while (n != 0) {
-            count += n & 1;
-            n >>>= 1;
-        }
-        return count;
+public int hammingWeight(int n) {
+    int count = 0;
+    while (n != 0) {
+        n &= (n - 1); 
+        count++;
     }
+    return count;
 }
